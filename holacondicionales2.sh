8b9c20387f43fb9c68da537d15a3f9867a1919ae
NEBULA1978 <rpascualey@gmail.com>
2023-03-12 01:46:14 +0100

    Practicando contadores y expresiones regulares con css

---
+++ holacondicionales2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Comparaciones con numeros
+# WORD=8
+# if [ $WORD -eq 10 ]; then #si son igual WORD=8 y el valor 10
+# if [ $WORD -ne 10 ]; then #si no son igual
+# if [ $WORD -lt 10 ]; then #si es menor
+# if [ $WORD -gt 10 ]; then #si es mayor
+# if [ $WORD -le 10 ]; then #si es menor o igual
+# if [ $WORD -ge 5 ]; then #si es mayor o igual
+#     echo "Run is fun!!"
+# else
+#     echo "--------"
+
+# fi
+
+# Comparaciones strings
+WORD="run"
+if [ $WORD == "run" ]; then
+    echo "Run is fun!!"
+else
+    echo "--------"
+
+fi
+
+# Ejemplo1
+# while true; do
+#     echo "Presiona Cotrl + C para salir"
+#     sleep 3s
+# done
+
+
+# WORD="run
+# echo $WORD
+# echo "${WORD}ning is fun!"
+# echo '${WORD}ning is fun!'
+# echo "===================="
+
+# Ejemplo2
+CONTADOR=0
+
+#Cuando sea menor o igual
+while [ $CONTADOR -lt 6 ]; do
+    if [ $CONTADOR -eq 0 ]; then
+        echo "El contador es cero"
+    else
+        echo "El contador es diferente a cero"
+    fi
+    CONTADOR=$(($CONTADOR + 1))
+    sleep 3s
+done
